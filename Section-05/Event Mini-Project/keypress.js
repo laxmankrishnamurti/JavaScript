08d547ef4298c644eeba648aa9471e2728563326
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-01-29 20:40:38 +0530

    Adding keypressing Project

--- Section-05/Event Mini-Project/keypress.js
+++ Section-05/Event Mini-Project/keypress.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+const input = document.querySelector('#input');
+let display = document.getElementById('display');
+
+function keyFinder(e){
+    // const key = e.code;
+    // display.textContent += key;
+    // display.textContent += `${e.code}`
+    // display.value = `${e.code}`
+    display.value += ` ${e.code}`
+}
+
+input.addEventListener('keypress', keyFinder);
