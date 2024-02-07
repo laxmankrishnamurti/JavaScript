1eb93b32eff0549300b3c063d3b6ee093098a397
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-02-08 01:51:47 +0530

    Adding new files

--- Section-06(Advance Module)/Promises/Why/callbackHell.js
+++ Section-06(Advance Module)/Promises/Why/callbackHell.js
@@ -22,21 +22,21 @@ const eight = document.querySelector('.eight');
 const nine = document.querySelector('.nine');
 const ten = document.querySelector('.ten');
 
-function changeText(element, text, color, time, onSuccess, onFailure){
-    setTimeout(() => {
-        if(element){
-            element.textContent = text;
-            element.style.color = color;
-            if(onSuccess){
-                onSuccess();
-            }
-        }else{
-            if(onFailure){
-                onFailure();
-            }
-        }
-    }, time);
-}
+// function changeText(element, text, color, time, onSuccess, onFailure){
+//     setTimeout(() => {
+//         if(element){
+//             element.textContent = text;
+//             element.style.color = color;
+//             if(onSuccess){
+//                 onSuccess();
+//             }
+//         }else{
+//             if(onFailure){
+//                 onFailure();
+//             }
+//         }
+//     }, time);
+// }
 
 // changeText(one, 'One', 'salmon', 1000, () => {
 //     console.log('Success');
@@ -56,7 +56,6 @@ function changeText(element, text, color, time, onSuccess, onFailure){
 //                             changeText(eight, 'Eight', 'white', 2000, () => {
 //                                 changeText(nine, 'Nine', 'white', 1000, () => {
 //                                     changeText(ten, 'Ten', 'white', 2000, () => {
-// PYRAMID OF DOM || CALLBACK HELL....................................................................
 //                                     }, () => {console.log(`Input element dosen't exist`)})
 //                                 }, () => {console.log(`Input element dosen't exist`)})
 //                             }, () => {console.log(`Input element dosen't exist`)})
@@ -68,6 +67,32 @@ function changeText(element, text, color, time, onSuccess, onFailure){
 //     }, () => {console.log(`Input element dosen't exist`)})
 // }, () => {console.log(`Input element dosen't exist`)})
 
-const collectChild = document.querySelectorAll('.child');
-const childGroup = Array.from(collectChild);
+//Let's do it using Promises
 
+function changeText(element, text, color, time, onSuccess, onFailure){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(element){
+                element.textContent = text;
+                element.style.color = color;
+                if(onSuccess){
+                    resolve(onSuccess());
+                }
+            }else{
+                if(onFailure){
+                    reject(onFailure());
+                }
+            }
+        }, time);
+    })
+}
+changeText(one, 'One', 'white', 1000, () => {changeText()}, () => {console.log(`The element dosen't exist`)})
+.then(data => changeText(two, 'Two', 'white', 1000, () => {changeText()}))
+.then(data => changeText(three, 'Three', 'white', 1000, () => {changeText()}))
+.then(data => changeText(four, 'four', 'white', 1000, () => {changeText()}))
+.then(data => changeText(five, 'five', 'white', 1000, () => {changeText()}))
+.then(data => changeText(six, 'six', 'white', 1000, () => {changeText()}))
+.then(data => changeText(seven, 'seven', 'white', 1000, () => {changeText()}))
+.then(data => changeText(eight, 'eight', 'white', 1000, () => {changeText()}))
+.then(data => changeText(nine, 'nine', 'white', 1000, () => {changeText()}))
+.then(data => changeText(ten, 'ten', 'white', 1000, () => {changeText()}))
