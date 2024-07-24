3920a35b1f77ad4b2957b54274de85cd68df7c8f
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-24 11:54:25 +0530

    Task - 05 is completed:

--- Section-10(30 Day JS Challenges)/Day-11/promises.js
+++ Section-10(30 Day JS Challenges)/Day-11/promises.js
@@ -57,6 +57,10 @@ const promiseTwo = new Promise(function (resolve, reject) {
 
 //Task - 03
 
+/*
+
+//Un-optimized code 
+
 function getUserData(id) {
   return new Promise(function (resolve, reject) {
     try {
@@ -131,3 +135,82 @@ getUserData(1)
   .catch((error) => {
     console.log("Promise rejected : ", error);
   });
+
+  */
+
+//Optimized Code
+
+async function getUserData(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/users/${id}`);
+    if (response.status === 200) {
+      const userData = await response.json();
+      return userData.username;
+    } else {
+      console.log("Response was not ok");
+      return null;
+    }
+  } catch (error) {
+    console.log("Error while fetching userdata :: ", error);
+  }
+}
+
+async function fetchUserNames() {
+  for (let i = 1; i <= 5; i++) {
+    try {
+      let username = await getUserData(i);
+      console.log(`Id : ${i} :: Username : ${username}`);
+    } catch (error) {
+      console.log(`Promise rejected for id : ${id}`);
+    }
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+}
+
+// fetchUserNames();
+
+/**
+ * Activity - 03
+ */
+
+//Task - 04
+
+async function getPromise(promise) {
+  try {
+    const promiseResolvedValue = await promise;
+    console.log("promiseResolvedValue : ", promiseResolvedValue);
+  } catch (error) {
+    console.error("Promise rejected :: ", error);
+  }
+}
+
+const promiseThree = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is resolved value");
+  }, 2000);
+});
+
+// getPromise(promiseThree);
+
+//Task - 05
+
+async function getPromise(promise) {
+  try {
+    let promiseRejectedValue = await promise;
+    console.log("promiseRejctedValue : ", promiseRejectedValue);
+  } catch (error) {
+    console.log("Promise rejected : ", error);
+  }
+}
+
+const promiseFour = new Promise((resolve, reject) => {
+  try {
+    setTimdfeout(() => {
+      resolve("promise resolve : true");
+    }, 2000);
+  } catch (error) {
+    reject("promise resolve : false");
+  }
+});
+
+getPromise(promiseFour);
