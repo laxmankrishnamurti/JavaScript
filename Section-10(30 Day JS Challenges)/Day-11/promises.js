6e2871b3e79c92f62cc45800982b8d153d61c0e3
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-24 10:58:23 +0530

    Day-11 challenge-03 has been completed:

--- Section-10(30 Day JS Challenges)/Day-11/promises.js
+++ Section-10(30 Day JS Challenges)/Day-11/promises.js
@@ -5,23 +5,23 @@
 //Task - 01
 
 const promiseOne = new Promise(function (resolve, reject) {
-    try {
-        setTimeout(() => {
-            console.log("Async task-01 has been completed")
-            resolve()
-        }, 2000);
-    } catch (error) {
-        reject()
-    }
-})
+  try {
+    // setTimeout(() => {
+    //   console.log("Async task-01 has been completed");
+    //   resolve();
+    // }, 2000);
+  } catch (error) {
+    reject();
+  }
+});
 
-promiseOne
-    .then(() => {
-        console.log("Promise consumed")
-    })
-    .catch((error) => {
-        console.error("Promise rejected with :: ", error)
-    })
+// promiseOne
+//   .then(() => {
+//     console.log("Promise consumed");
+//   })
+//   .catch((error) => {
+//     console.error("Promise rejected with :: ", error);
+//   });
 
 //Output : Async task has been completed
 //Output : Promise consumed
@@ -29,24 +29,24 @@ promiseOne
 //Task - 02
 
 const promiseTwo = new Promise(function (resolve, reject) {
-    try {
-        setTimeout(() => {
-            const response = fetch('http://localhost:3000/accounts')
-            console.log("Async task - 02 has been completed")
-            resolve(response)
-        }, 2000);
-    } catch (error) {
-        reject()
-    }
-})
+  try {
+    // setTimeout(() => {
+    //   const response = fetch("http://localhost:3000/accounts");
+    //   console.log("Async task - 02 has been completed");
+    //   resolve();
+    // }, 2000);
+  } catch (error) {
+    reject();
+  }
+});
 
-promiseTwo
-    .then(() => {
-        console.log("promiseTwo consumed successfully")
-    })
-    .catch((error) => {
-        console.error("promiseTwo rejected ::", error)
-    })
+// promiseTwo
+//   .then(() => {
+//     console.log("promiseTwo consumed successfully");
+//   })
+//   .catch((error) => {
+//     console.error("promiseTwo rejected ::", error);
+//   });
 
 //Output : promiseTwo rejected
 //Output : TypeError: Failed to Fetch
@@ -56,3 +56,78 @@ promiseTwo
  */
 
 //Task - 03
+
+function getUserData(id) {
+  return new Promise(function (resolve, reject) {
+    try {
+      setTimeout(() => {
+        fetch(`http://localhost:3000/users/${id}`)
+          .then((response) => {
+            if (response.status === 200) {
+              return response.json();
+            } else {
+              console.log("Error while fetching userdata");
+            }
+          })
+          .then((data) => {
+            let { username } = data;
+            resolve(username);
+          });
+      }, 1000);
+    } catch (error) {
+      reject();
+    }
+  });
+}
+
+getUserData(1)
+  .then((data) => {
+    console.log("username is : ", data);
+  })
+  .then(() => {
+    setTimeout(() => {
+      getUserData(2)
+        .then((data) => {
+          console.log("username is : ", data);
+        })
+        .catch((error) => {
+          console.log("Promise rejected : ", error);
+        });
+    }, 2000);
+  })
+  .then(() => {
+    setTimeout(() => {
+      getUserData(3)
+        .then((data) => {
+          console.log("username is : ", data);
+        })
+        .catch((error) => {
+          console.log("Promise rejected : ", error);
+        });
+    }, 3000);
+  })
+  .then(() => {
+    setTimeout(() => {
+      getUserData(4)
+        .then((data) => {
+          console.log("username is : ", data);
+        })
+        .catch((error) => {
+          console.log("Promise rejected : ", error);
+        });
+    }, 4000);
+  })
+  .then(() => {
+    setTimeout(() => {
+      getUserData(5)
+        .then((data) => {
+          console.log("username is : ", data);
+        })
+        .catch((error) => {
+          console.log("Promise rejected : ", error);
+        });
+    }, 5000);
+  })
+  .catch((error) => {
+    console.log("Promise rejected : ", error);
+  });
