8ac060d6acbc5f0479c2b7ee1a4f39a3c061a7cf
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-24 09:07:23 +0530

    First challenge is completed

--- Section-10(30 Day JS Challenges)/Day-11/promises.js
+++ Section-10(30 Day JS Challenges)/Day-11/promises.js
@@ -4,4 +4,24 @@
 
 //Task - 01
 
-console.log("Hello world!");
+const promiseOne = new Promise(function (resolve, reject) {
+    try {
+        setTimeout(() => {
+            console.log("Async task has been completed")
+            resolve()
+        }, 2000);
+    } catch (error) {
+        reject()
+    }
+})
+
+promiseOne
+    .then(() => {
+        console.log("Promise consumed")
+    })
+    .catch((error) => {
+        console.error("Promise rejected with :: ", error)
+    })
+
+//Output : Async task has been completed
+//Output : Promise consumed
