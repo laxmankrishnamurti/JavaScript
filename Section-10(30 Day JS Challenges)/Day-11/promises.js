4666083f37d5858bc7f5b0e1d0819bd41f695082
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-24 09:44:01 +0530

    Day-11 challenge - 2 has been completed

--- Section-10(30 Day JS Challenges)/Day-11/promises.js
+++ Section-10(30 Day JS Challenges)/Day-11/promises.js
@@ -7,7 +7,7 @@
 const promiseOne = new Promise(function (resolve, reject) {
     try {
         setTimeout(() => {
-            console.log("Async task has been completed")
+            console.log("Async task-01 has been completed")
             resolve()
         }, 2000);
     } catch (error) {
@@ -25,3 +25,34 @@ promiseOne
 
 //Output : Async task has been completed
 //Output : Promise consumed
+
+//Task - 02
+
+const promiseTwo = new Promise(function (resolve, reject) {
+    try {
+        setTimeout(() => {
+            const response = fetch('http://localhost:3000/accounts')
+            console.log("Async task - 02 has been completed")
+            resolve(response)
+        }, 2000);
+    } catch (error) {
+        reject()
+    }
+})
+
+promiseTwo
+    .then(() => {
+        console.log("promiseTwo consumed successfully")
+    })
+    .catch((error) => {
+        console.error("promiseTwo rejected ::", error)
+    })
+
+//Output : promiseTwo rejected
+//Output : TypeError: Failed to Fetch
+
+/**
+ * Activity - 02
+ */
+
+//Task - 03
