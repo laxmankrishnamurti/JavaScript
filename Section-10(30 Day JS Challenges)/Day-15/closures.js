0640d7df685f13b3755166410434fd4eac2bfc3a
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-07-27 22:01:27 +0530

    Task-07 completed

--- Section-10(30 Day JS Challenges)/Day-15/closures.js
+++ Section-10(30 Day JS Challenges)/Day-15/closures.js
@@ -110,13 +110,114 @@ greets();
 
 function closureInLoop() {
   let arrayOfFunctions = [];
+  let names = ["Laxman", "Kawya", "Pallavi"];
   for (let i = 0; i < 3; i++) {
-    arrayOfFunctions.push(function () {
-      console.log(`I'm on ${i} position`);
-    });
+    arrayOfFunctions.push(
+      (function (index) {
+        return function () {
+          console.log(`I'm on ${index} position with name :: ${names[i]}`);
+        };
+      })(i)
+    );
   }
 
   return arrayOfFunctions;
 }
 
 let allFunctions = closureInLoop();
+allFunctions[0]();
+allFunctions[1]();
+allFunctions[2]();
+
+/**
+ * Activity - 04
+ */
+
+//Task - 06
+
+function itemManager() {
+  let items = [];
+
+  return {
+    addItem: function (item) {
+      items.push(item);
+      console.log(`Added ${item}`);
+    },
+
+    removeItem: function (item) {
+      let itemPosition = items.indexOf(item);
+      if (itemPosition) {
+        items.splice(itemPosition, 1);
+        console.log(`Removed : ${item}`);
+      } else {
+        console.log(`${item} not found`);
+      }
+    },
+
+    getAllItems: function () {
+      console.log(`All items : ${items}`);
+    },
+  };
+}
+
+const manager = itemManager();
+manager.addItem("Goggles");
+manager.addItem("Chair");
+manager.addItem("Books");
+manager.addItem("Mouse");
+manager.getAllItems();
+
+manager.removeItem("Chair");
+manager.getAllItems();
+
+/**
+ * Avtivit - 05
+ */
+
+//Task - 07
+
+function memoize(fun) {
+  let cache = {};
+
+  return function (args) {
+    let key = JSON.stringify(args);
+
+    if (cache[key] !== undefined) {
+      console.log(`Fetching from cache for agrs : ${args}`);
+      return cache[args];
+    }
+
+    let result = fun(args);
+    cache[key] = result;
+
+    console.log(`Computing result for args : ${args}`);
+    return result;
+  };
+}
+
+function getFactorial(num) {
+  if (num == 1) {
+    return 1;
+  }
+
+  let factorial = 1;
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+
+  return factorial;
+}
+
+const memoizedFunction = memoize(getFactorial);
+let result = memoizedFunction(5);
+console.log(`Factorial of 5 is : ${result}`);
+let result2 = memoizedFunction(10);
+console.log(`Factorial of 10 is : ${result}`);
+let result3 = memoizedFunction(15);
+console.log(`Factorial of 15 is : ${result}`);
+
+let result4 = memoizedFunction(15);
+console.log(`Factorial of 15 is : ${result}`);
+
+let result5 = memoizedFunction(5);
+console.log(`Factorial of 5 is : ${result}`);
